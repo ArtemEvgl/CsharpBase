9e8dd33c3a04b562394db54f02f82593e9760b34
ArtemEvgl <Usbhawk5@gmail.com>
2020-09-21 22:40:24 +0300

    Lesson08.2 and Lesson08.3

---
+++ Lesson08.2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson08._2
+{
+
+    enum EnumColors
+    {
+        Red = 1,
+        Blue,
+        Yellow
+    }
+    static class OutputData
+    {
+        public static void Print(string data, int color)
+        {
+            switch(color)
+            {
+                case (int)EnumColors.Blue:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case (int)EnumColors.Red:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case (int)EnumColors.Yellow:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                default:
+                    Console.WriteLine("Number of color not found. Console color is default (gray)");
+                    break;
+            }
+            Console.WriteLine(data);
+            Console.ResetColor();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the data:");
+            string data = Console.ReadLine();
+            Console.WriteLine("Choose the number of color:");
+            Console.WriteLine("1. Red");
+            Console.WriteLine("2. Blue");
+            Console.WriteLine("3. Yellow");
+            int color = Convert.ToInt32(Console.ReadLine());
+            OutputData.Print(data, color);
+            Console.ReadKey();
+        }
+
+        
+    }
+}
---
+++ Lesson08.3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson08._3
+{
+
+    enum Post
+    {
+        Boss = 10,
+        Developer = 40,
+        Manager = 80,
+        Security = 120
+    }
+    class Accauntant
+    {
+        public static bool AskForBonus(Post worker, int hours)
+        {
+            return (int)worker < hours;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter post:");           
+            string post = Console.ReadLine().ToLower();
+            int workHours;
+            while (true)
+            {
+                Console.WriteLine("Enter work hours:");
+                if (int.TryParse(Console.ReadLine(), out workHours))
+                    break;
+                else
+                    Console.WriteLine("Input data is incorrect. Please try again");
+            }                        
+            Post worker = (Post)Enum.Parse(typeof(Post), post, true);
+            bool bonusResult = Accauntant.AskForBonus(worker, workHours);
+            if(bonusResult)
+            {
+                Console.WriteLine("BONUS!!! LUCKY CHEAL!");
+            } else
+            {
+                Console.WriteLine("No Bonus. No money, no honey");
+            }
+            Console.ReadKey();
+        }
+    }
+}

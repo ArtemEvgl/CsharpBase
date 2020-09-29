595755c158acd45e95d48922486ed51d3f852a92
ArtemEvgl <Usbhawk5@gmail.com>
2020-09-29 22:56:35 +0300

    Lesson13 (13.1 and 13.2)

---
+++ Lesson13.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson13._1
+{
+
+    
+    class Program
+    {
+        static int deep;
+        
+        public static void Recursion()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} say hello");
+            Thread thread = new Thread(Recursion) { Name = "Thread " + (++deep) };
+            Thread.Sleep(500);
+            thread.Start();
+        }
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(Recursion) { Name = "Thread " + deep };
+            thread.Start();
+        }
+    }
+}
---
+++ Lesson13.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson13._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.SetWindowSize(80, 40);
+
+            Matrix matrix;
+            for (int i = 0; i < 30; i++)
+            {
+                matrix = new Matrix(i * 30);
+                new Thread(matrix.Move).Start();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}

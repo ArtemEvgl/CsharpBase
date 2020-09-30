42fee5247a4660795ef101578e97fdb81ff1d8e3
ArtemEvgl <Usbhawk5@gmail.com>
2020-09-30 23:48:06 +0300

    Lesson 14.1

---
+++ Lesson14.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson14._1
+{
+    class Program
+    {
+        
+        static IEnumerable FindEven(int[] arr)
+        {
+            if(arr.Length >= 0)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] % 2 == 0)
+                    {
+                        yield return arr[i];
+                    }
+                }
+            } else
+            {
+                yield break;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            foreach (var item in FindEven(array))
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
